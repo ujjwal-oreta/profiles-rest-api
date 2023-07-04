7d69d0bba880e36a26731ba646b6a60a792e4d33
Ujjwal Sharma <ujjwal@oreta.com.au>
2023-07-04 14:58:20 +0530

    Create Hello APIView

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    "Test API View"
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP methods as functions (get, post, put, patch, delete)',
+            'Is similar to a traditional Django View'
+        ]
+        return Response({'Message': 'Hello!!', 'an_apiview': an_apiview})
