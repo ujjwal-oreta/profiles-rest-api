cdfafd6a881757bfed2dbfb668094defc64b901d
Ujjwal Sharma <ujjwal@oreta.com.au>
2023-07-04 16:05:40 +0530

    Added post, put and delete methods

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
     "Test API View"
+    serializers_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Returns a list of APIView features"""
@@ -12,3 +15,19 @@ class HelloApiView(APIView):
             'Is similar to a traditional Django View'
         ]
         return Response({'Message': 'Hello!!', 'an_apiview': an_apiview})
+    
+
+    def post(self, request):
+        """Create a Hello Message with our name"""
+        serializer = self.serializers_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST    
+            )
