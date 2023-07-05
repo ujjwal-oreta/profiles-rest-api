9505b9dbc0cecf2c8961db6df48d75229ef723ac
Ujjwal Sharma <ujjwal@oreta.com.au>
2023-07-05 12:52:28 +0530

    Adding search and permissions to User profile view set

--- profiles_project/profiles_api/views.py
+++ profiles_project/profiles_api/views.py
@@ -2,8 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
+
 
 class HelloApiView(APIView):
     "Test API View"
@@ -64,3 +69,14 @@ class HelloViewSet(viewsets.ViewSet):
     def retrieve(self, request, pk=None):
         """Handle getting an object by its ID"""
         return Response({'http_method': 'GET'})
+    
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating Profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
