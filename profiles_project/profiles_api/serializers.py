eaf1fbb88f7cc59e944635d7438a3f13d79c87f8
Ujjwal Sharma <ujjwal@oreta.com.au>
2023-07-05 11:11:24 +0530

    Adding viewsets to the profilesapi

--- profiles_project/profiles_api/serializers.py
+++ profiles_project/profiles_api/serializers.py
@@ -4,4 +4,3 @@ from rest_framework import serializers
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
-    
